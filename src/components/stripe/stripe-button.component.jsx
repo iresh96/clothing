ddd1a0bc00aa7fadcbebf7c395b999e63fdd6785
Iresh Madushanka <ireshmadushanka03@gmail.com>
2021-01-20 00:27:38 +0530

    intergrated stripe frontend

---
+++ src/components/stripe/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+
+  const publishableKey =
+    "pk_test_51IBOoGBvRlJimRYCOihtrHG3EWYuFI1FIiqIQeMDuGKdEWy0ZZqngJTUM7fRwhWrRgGPQPXEGHSw0Scy7YLBmvhd00wTOxe6nL";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN CLOTHING PVT LTD"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your Total Is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      stripeKey={publishableKey}
+      token={onToken}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
